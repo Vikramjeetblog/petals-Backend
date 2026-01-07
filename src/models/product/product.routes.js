f566cd28f5ae901c64b256caba5d569f40f6762e
Vikramjeetblog <89984187+Vikramjeetblog@users.noreply.github.com>
2026-01-07 11:17:19 +0530

    Add files via upload

---
+++ src/models/product/product.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// ✅ correct relative path
+const productController = require("./product.controller");
+
+router.post("/", productController.createProduct);
+router.get("/", productController.getProducts);
+router.put("/:id", productController.updateProduct);
+
+module.exports = router;
