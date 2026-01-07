58452e69b97e97ad5bc8186bc0d2b9a88693810e
Vikramjeetblog <89984187+Vikramjeetblog@users.noreply.github.com>
2026-01-07 18:41:31 +0530

    Add authMiddleware to cart route handlers

--- src/models/cart/cart.routes.js
+++ src/models/cart/cart.routes.js
@@ -7,16 +7,16 @@ const cartController = require('./cart.controller');
 router.post('/add', authMiddleware, cartController.addToCart);
 
 // View cart (express + marketplace split)
-router.get('/view',  cartController.viewCart);
+router.get('/view', authMiddleware,  cartController.viewCart);
 
 // Update quantity (0 = remove)
 router.post('/update',  authMiddleware,cartController.updateQuantity);
 
 // Remove item explicitly
-//router.post('/remove', authMiddleware, cartController.removeItem);
+router.post('/remove', authMiddleware, cartController.removeItem);
 
 // Merge guest cart into user cart
-//router.post('/merge', authMiddleware,  cartController.mergeCart);
+router.post('/merge', authMiddleware,  cartController.mergeCart);
 
 module.exports = router;
 
