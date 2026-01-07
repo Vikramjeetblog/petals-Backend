c41d824dd68d97f8933c184d675a0fcfd14c9be6
Vikramjeetblog <89984187+Vikramjeetblog@users.noreply.github.com>
2026-01-07 20:02:15 +0530

    Delete src/models/vendor.model.js

--- src/models/vendor.model.js
+++
@@ -1,8 +0,0 @@
-// src/models/vendor/vendor.model.js
-const mongoose = require('mongoose');
-
-const VendorSchema = new mongoose.Schema({
-  name: String
-});
-
-module.exports = mongoose.model('Vendor', VendorSchema);
