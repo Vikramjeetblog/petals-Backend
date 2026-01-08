7b12349b1a0057ceb944d3c5dab166bf1488d9b5
Vikramjeetblog <89984187+Vikramjeetblog@users.noreply.github.com>
2026-01-08 11:42:50 +0530

    Add getProductById route to product.routes.js

--- src/models/product/product.routes.js
+++ src/models/product/product.routes.js
@@ -6,6 +6,8 @@ const productController = require("./product.controller");
 
 router.post("/", productController.createProduct);
 router.get("/", productController.getProducts);
+router.get("/:id", productController.getProductById);
 router.put("/:id", productController.updateProduct);
 
 module.exports = router;
+
