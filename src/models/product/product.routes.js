34d9117f16fce7529f9afec8f16bd608fd35705d
Vikramjeetblog <89984187+Vikramjeetblog@users.noreply.github.com>
2026-01-09 14:18:03 +0530

    Reorder route definitions in product.routes.js

--- src/models/product/product.routes.js
+++ src/models/product/product.routes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-// ✅ correct relative path
 const productController = require("./product.controller");
 
-router.post("/", productController.createProduct);
+router.get('/search', productController.searchProducts);
 router.get("/", productController.getProducts);
 router.get("/:id", productController.getProductById);
 router.put("/:id", productController.updateProduct);
-router.get('/search', productController.searchProducts);
+router.post("/", productController.createProduct);
+
 module.exports = router;
+
 
 
