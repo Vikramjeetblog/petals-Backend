d419254219a84dd0d3503e88f52308c24313adee
Vikramjeetblog <89984187+Vikramjeetblog@users.noreply.github.com>
2026-01-23 16:39:29 +0530

    Delete src/models/checkout directory

--- src/models/checkout/checkout.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require('../middleware/auth.middleware');
-const { checkout } = require('./checkout.controller');
-
-router.post('/', authMiddleware, checkout);
-
-module.exports = router;
