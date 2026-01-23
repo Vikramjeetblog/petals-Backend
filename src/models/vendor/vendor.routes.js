470dc9e5b77de2dc451b161ceaae8c218c0cf71b
Vikramjeetblog <89984187+Vikramjeetblog@users.noreply.github.com>
2026-01-23 18:14:36 +0530

    Update vendor.routes.js

--- src/models/vendor/vendor.routes.js
+++ src/models/vendor/vendor.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const auth = require('../middleware/auth.middleware');
 const vendorCandidateOnly = require('../middleware/vendorCandidateOnly.middleware');
-const VendorOnly = require('../middleare/VendorOnly.middleware');
+const VendorOnly = require('../middleware/VendorOnly.middleware');
 // 🔥 Import split vendor controllers (barrel file)
 const vendor = require('./index');
 
@@ -45,5 +45,6 @@ router.get('/earnings', auth, vendorOnly, vendor.earnings.getEarnings);
 router.post('/earnings/payout', auth, vendorOnly, vendor.earnings.requestPayout);
 
 module.exports = router;
+
 
 
