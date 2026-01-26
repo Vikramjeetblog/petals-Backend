ee9ddd5ce6c54b405afcfda03d751bac65194a81
Vikramjeetblog <89984187+Vikramjeetblog@users.noreply.github.com>
2026-01-26 17:44:10 +0530

    Update product.routes.js

--- src/models/product/product.routes.js
+++ src/models/product/product.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-
-//  correct relative path
+const auth = require("../middleware/auth");
 const productController = require("./product.controller");
+
+router.use(auth);
 
 router.post("/", productController.createProduct);
 router.get("/", productController.getProducts);
 router.put("/:id", productController.updateProduct);
+router.patch("/:id/toggle", productController.toggleProduct);
 
 module.exports = router;
